db547531c7f1ef64f8526f283a5c551ad4280afa
Kartoffelliebhaber <93953332+Kartoffelliebhaber@users.noreply.github.com>
2022-02-13 14:57:31 +0100

    Create fizzbuzz.cs

---
+++ fizzbuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool in_game = true;
+            while (in_game) {
+
+                for (int i = 1; i < 100; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+
+
+                        Console.WriteLine("Ismételjem (y/n)?");
+                        string repeat_input = Console.ReadLine();
+                        if (repeat_input == "n")
+                            in_game = false;
+                    }
+                }
+            }
+        }
+    }
+}
